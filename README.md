f03fca0d696fd1de91eb84c723fe75dc35c0387b
Josh Suarez <joshuasuarez@gmail.com>
2019-08-20 15:50:20 -0400

    Used an rdd for the frequency dictionary instead of a regular dictionary to save on memory.
    Added readme.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # jumble-solver
 A pyspark script that will attempt to solve newspaper Jumble puzzles
+
+## To Run
+
+This was run on Ubuntu for Windows Subystem. I used the script `spark-submit.sh` in order to set up spark on my local computer and run the python script via `spark-submit`. My script's output can be grepped out of the command line by filtering for 'Jumble Solver:' lines.
+
+## Output
+
+My script attempts to solve the jumbles by looking for words with the lowest score that is not 0. If none are available, then whatever is available is ued. For the final jumble, I attempt to use all combinations of circled letters of all possible answers and then try to find the lowest score that is not zero for the final answer. For some puzzles this works (Puzzle 3: Addition) but for others it is close enough or completely wrong (Puzzle 1: "Job Need Wall" instead of "Jab Well Done").
+
+```
+Jumble Solver: jumbles: {'WRALEY': (1, 3, 5), 'RAMOJ': (3, 4), 'NAGLD': (2, 4, 5), 'CAMBLE': (1, 2, 4)}
+Jumble Solver: best answers: lawyer major gland becalm
+Jumble Solver: best final answer: job need wall
+Jumble Solver: jumbles: {'BNEDL': (1, 5), 'SEHEYC': (2, 6), 'IDOVA': (1, 4, 5), 'ARACEM': (2, 5, 6)}
+Jumble Solver: best answers: blend cheesy avoid camera
+Jumble Solver: best final answer: bad hair day
+Jumble Solver: jumbles: {'DOORE': (1, 2, 4), 'DITNIC': (1, 2, 3), 'CATILI': (1, 3, 6), 'SHAST': (1, 4, 5)}
+Jumble Solver: best answers: rodeo indict italic stash
+Jumble Solver: best final answer: dish scenario
+Jumble Solver: jumbles: {'CRONEE': (2, 4), 'LEGIA': (1, 3), 'KNIDY': (1, 2), 'TUVEDO': (1, 6)}
+Jumble Solver: best answers: encore agile dinky devout
+Jumble Solver: best final answer: addition
+Jumble Solver: jumbles: {'CEEDIT': (2, 4, 6), 'DRIVET': (3, 6), 'SNAMEA': (1, 6), 'ELCHEK': (2, 6), 'SOWDAH': (1, 4), 'GYRINT': (1, 2, 4)}
+Jumble Solver: best answers: deceit divert seaman kechel shadow trying
+Jumble Solver: best final answer: divert tentless
+```
--- jumble_solver.py
+++ jumble_solver.py
@@ -7,10 +7,13 @@ import re
 spark = SparkSession.builder.appName("Jumble Solver").getOrCreate()
 sc = spark.sparkContext
 
-#Load the json data into a dictionary
-freq_dict = {}
-with open("freq_dict.json", 'r') as jsonFile:
-	freq_dict = json.load(jsonFile)
+#Load the dictionary into an RDD of tuple( word, score )
+freq_dict_df = sc.textFile('freq_dict.json', minPartitions=100) \
+	.filter(lambda line: ':' in line) \
+	.map(lambda line: ( re.sub("[\s\",]+", "", line), ) ) \
+	.map(lambda line: ( line[0].split(":")[0], int(line[0].split(":")[1]) ) ) \
+	.toDF() \
+	.rdd
 	
 #Helper function to get all words of length n (wordLength) from the dictionary 
 #Runs fast enough with this data set, in production this would work better as a spark dataframe
@@ -37,28 +40,24 @@ for puzzle in puzzleInputs:
 	#Loop through the keys and values in jumbles
 	#Keys are jumbled letters, values are indices of circled letters
 	for (key,val) in jumbles.items():
-		#Get all unique permutations of the jumble letter set
-		letterSetPermutations = set(permutations(key))
-		
-		#Get all words in the dictionary that are the same length as the jumble
-		#  -The filter removes words with characters not in the jumble
+	
+		#Get all words in the freq dictionary that are the same length as the jumble
+		#  -The second filter removes words with characters not in the jumble
 		#  -map creates a tuple of (word, frequency_score, string of circled characters)
 		#  -sort will assign a value of 'infinity' to words with score of 0 since they are infrequent. This
 		#   will make them last in the ordering
-		possibleWordResults = sc \
-			.parallelize( getWordsFromDictionary(len(key)) , 16 ) \
-			.filter( lambda word: checkIfWordInAnsStr(word, key.lower()) ) \
-			.map( lambda word : (word, freq_dict.get(word), "".join([ list(word)[i-1] for i in val ])) ) \
+		possibleWordResults = freq_dict_df \
+			.filter( lambda word: len(word[0]) == len(key) ) \
+			.filter( lambda word: checkIfWordInAnsStr(word[0], key.lower()) ) \
+			.map( lambda word : (word[0], word[1], "".join([ list(word[0])[i-1] for i in val ])) ) \
 			.sortBy( lambda wordTup: float('inf') if wordTup[1] == 0 else wordTup[1] ) \
 			.collect()
 		
 		#keep track of all the possible word results for each jumble. This will be a list of list of tuples.
 		circledLettersAns.append(possibleWordResults)
 	
-
 	#Begin searching for the final answer
 	foundFinalAnswers = []    #possible final answers are stored in this list
-	loopCount = 1             #loop count is used for determining whether to filter out words later
 	
 	#the itertools.product function will go through every combination in the list of possible answers
 	for answersPermutation in product(*circledLettersAns):
@@ -78,15 +77,26 @@ for puzzle in puzzleInputs:
 		#couldn't exist together in the answer
 		possibleAnswers = []
 		for ansLen in answerLengths:
-			possibleWords = getWordsFromDictionary(ansLen)
+		
+			#Check to see if all words of this length have already been grabbed
+			skip = False
+			if len(possibleAnswers) > 0:
+				for wordList in possibleAnswers:
+					if len(wordList[0]) == ansLen:
+						possibleAnswers.append(wordList)
+						skip = True
+						break
+			
+			if skip:
+				continue
 			
 			#First filter by if the word is in the answer string
 			#Next map to (word, frequency_score)
 			#Then sort by frequency_score. Again, 0s are set to 'inf' and made last in ascending sort
-			ansContains = sc \
-				.parallelize( possibleWords, 16 ) \
-				.filter( lambda word : checkIfWordInAnsStr(word, ansStr) ) \
-				.map( lambda word : (word, freq_dict.get(word)) ) \
+			ansContains = freq_dict_df \
+				.filter( lambda word: len(word[0]) == ansLen ) \
+				.filter( lambda word : checkIfWordInAnsStr(word[0], ansStr) ) \
+				.map( lambda word : (word[0], word[1]) ) \
 				.sortBy( lambda wordTup: float('inf') if wordTup[1] == 0 else wordTup[1] ) \
 				.collect()
 			
@@ -95,6 +105,7 @@ for puzzle in puzzleInputs:
 			else:
 				possibleAnswers.append(ansContains)
 		
+		#print("mydebug: {}".format(possibleAnswers))
 		
 		#Now check the words to see if they fit in the final answer
 		if len(possibleAnswers) == len(answerLengths):
@@ -106,7 +117,7 @@ for puzzle in puzzleInputs:
 			#  -filter with checkIfWordInAnsStr again, this time using a joined string of all the possible answer words
 			#  -map to a tuple of (joined sentence, score, answers used to created the final sentence)
 			#  -sort by the best score
-			#  -take the top answer 				.filter( lambda sentence: True if loopCount > 1 and len(foundFinalAnswers) == 0 else sum(float('inf') if n == 0 else n for _,n in sentence) != float('inf') )\
+			#  -take the top answer
 			bestPossible = sc \
 				.parallelize( set(product(*possibleAnswers)), 1000) \
 				.filter( lambda sentence: checkIfWordInAnsStr("".join(list(map(lambda x : x[0], sentence))), ansStr[:]) ) \
@@ -117,12 +128,11 @@ for puzzle in puzzleInputs:
 			if len(bestPossible) > 0:
 				foundFinalAnswers.append(bestPossible[0])
 				
-		loopCount += 1
 
 	#Print out the final results
 	if len(foundFinalAnswers) > 0:
-		print("=====\nmydebug: jumbles: {}".format(jumbles))
-		print("mydebug: best answers: {}".format(" ".join(list(map(lambda wordScore: wordScore[0], foundFinalAnswers[0][2])))) )
-		print("mydebug: best final answer: {}".format(foundFinalAnswers[0][0]) )
+		print("=====\nJumble Solver: jumbles: {}".format(jumbles))
+		print("Jumble Solver: best answers: {}".format(" ".join(list(map(lambda wordScore: wordScore[0], foundFinalAnswers[0][2])))) )
+		print("Jumble Solver: best final answer: {}".format(foundFinalAnswers[0][0]) )
 	else:
-		print("mydebug: no answers found")
+		print("Jumble Solver: no answers found")
